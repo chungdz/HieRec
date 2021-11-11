83bd427ebc90f9c8dff9a6f2871de4548b2d0d47
xiaoxin Hu <1033718596@qq.com>
2021-11-11 10:00:37 -0800

    embd

--- prepocess/embd.py
+++ prepocess/embd.py
@@ -18,7 +18,7 @@ def build_entity_dict(emb_dict, lines, desc):
 word_embed_size = 300
 entity_embed_size = 100
 
-data_path = 'data'
+data_path = 'adressa'
 
 print('build word embeddings')
 word_dict = json.load(open(os.path.join(data_path, "word.json"), 'r', encoding='utf-8'))
@@ -49,35 +49,6 @@ for k, v in tqdm(word_dict.items(), total=len(word_dict), desc='generate word em
 print("Totally find {} words in pre-trained embeddings.".format(words_found))
 np.save(wemb_path, weights_matrix)
 print('word embedding shape:', weights_matrix.shape)
-
-
-print('build entity embeddings')
-entity_dict = json.load(open(os.path.join(data_path, "entity.json"), 'r', encoding='utf-8'))
-entity_train_path = os.path.join(data_path, "train/entity_embedding.vec")
-entity_dev_path = os.path.join(data_path, "dev/entity_embedding.vec")
-entity_test_path = os.path.join(data_path, "test/entity_embedding.vec")
-entity_emb_path = os.path.join(data_path, "entity_emb.npy")
-
-emb_dict = {}
-lines = open(entity_train_path, "r", encoding="utf8").readlines()
-build_entity_dict(emb_dict, lines, 'parse train entities')
-lines = open(entity_dev_path, "r", encoding="utf8").readlines()
-build_entity_dict(emb_dict, lines, 'parse dev entities')
-lines = open(entity_test_path, "r", encoding="utf8").readlines()
-build_entity_dict(emb_dict, lines, 'parse test entities')
-
-entity_weights_matrix = np.zeros((len(entity_dict), entity_embed_size))
-entities_found = 0
-
-for k, v in tqdm(entity_dict.items(), total=len(entity_dict), desc='generate entity embeddings'):
-    try:
-        entity_weights_matrix[v] = emb_dict[k]
-        entities_found += 1
-    except KeyError:
-        entity_weights_matrix[v] = np.random.normal(size=(entity_embed_size,))
-print("Totally find {} entities in pre-trained embeddings.".format(entities_found))
-np.save(entity_emb_path, entity_weights_matrix)
-print('entity embedding shape:', entity_weights_matrix.shape)
 
 print('build news title embeddings')
 news_dict = json.load(open(os.path.join(data_path, "news.json"), 'r', encoding='utf-8'))
