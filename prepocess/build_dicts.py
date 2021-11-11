279809f6bad44a3896390bbf66c193603e2bbf12
xiaoxin Hu <1033718596@qq.com>
2021-11-11 08:25:13 -0800

    build dict change

--- prepocess/build_dicts.py
+++ prepocess/build_dicts.py
@@ -34,27 +34,15 @@ def parse_his(user_dict, his_beh, desc, user_idx):
     
     return user_idx
 
-data_path = 'data'
+data_path = 'adressa'
 max_title_len = 30
 max_entity_len = 5
 
 print("Loading news info")
-f_train_news = os.path.join(data_path, "train/news.tsv")
-f_dev_news = os.path.join(data_path, "dev/news.tsv")
-f_test_news = os.path.join(data_path, "test/news.tsv")
-
-train_news = pd.read_csv(f_train_news, sep="\t", encoding="utf-8",
+news_df = pd.read_csv(os.path.join(data_path, "news.tsv"), sep="\t", encoding="utf-8",
                         names=["newsid", "cate", "subcate", "title", "abs", "url", "title_ents", "abs_ents"],
                         quoting=3)
-dev_news = pd.read_csv(f_dev_news, sep="\t", encoding="utf-8",
-                        names=["newsid", "cate", "subcate", "title", "abs", "url", "title_ents", "abs_ents"],
-                        quoting=3)
-test_news = pd.read_csv(f_test_news, sep="\t", encoding="utf-8",
-                        names=["newsid", "cate", "subcate", "title", "abs", "url", "title_ents", "abs_ents"],
-                        quoting=3)
-
-all_news = pd.concat([train_news, dev_news, test_news], ignore_index=True)
-all_news = all_news.drop_duplicates("newsid")
+all_news = news_df.drop_duplicates("newsid")
 
 news_dict = {}
 word_dict = {'<pad>': 0}
@@ -66,9 +54,8 @@ word_idx = 1
 news_idx = 2
 entity_idx = 1
 category_idx = 2
-subcategory_idx = 2
 
-for n, title, cate, subcate, title_ents, abs_ents in tqdm(all_news[['newsid', "title", "cate", "subcate", "title_ents", "abs_ents"]].values, total=all_news.shape[0], desc='parse news'):
+for n, title, cate, title_ents in tqdm(all_news[['newsid', "title", "cate", "title_ents"]].values, total=all_news.shape[0], desc='parse news'):
     news_dict[n] = {}
     news_dict[n]['idx'] = news_idx
     news_idx += 1
@@ -86,7 +73,7 @@ for n, title, cate, subcate, title_ents, abs_ents in tqdm(all_news[['newsid', "t
             wid_arr.append(word_dict['<pad>'])
     news_dict[n]['title'] = wid_arr[:max_title_len]
     
-    entity_arr = parse_ent_list(title_ents) + parse_ent_list(abs_ents)
+    entity_arr = parse_ent_list(title_ents)
     eidx_arr = set()
     for e in entity_arr:
         if e not in entity_dict:
@@ -105,11 +92,6 @@ for n, title, cate, subcate, title_ents, abs_ents in tqdm(all_news[['newsid', "t
         category_idx += 1
     news_dict[n]['category'] = category_dict[cate]
 
-    if subcate not in subcategory_dict:
-        subcategory_dict[subcate] = subcategory_idx
-        subcategory_idx += 1
-    news_dict[n]['subcate'] = subcategory_dict[subcate]
-
 ## paddning news for impression
 news_dict['<pad>']= {}
 news_dict['<pad>']['idx'] = 0
@@ -127,7 +109,6 @@ if cur_len < max_title_len:
 news_dict['<pad>']['title'] = wid_arr[:max_title_len]
 news_dict['<pad>']['entity'] = [entity_dict['<pad>']] * max_entity_len
 news_dict['<pad>']['category'] = category_dict['<pad>']
-news_dict['<pad>']['subcate'] = subcategory_dict['<pad>']
 
 ## paddning news for history
 news_dict['<his>']= {}
@@ -135,12 +116,10 @@ news_dict['<his>']['idx'] = 1
 news_dict['<his>']['title'] = [word_dict['<pad>']] * max_title_len
 news_dict['<his>']['entity'] = [entity_dict['<pad>']] * max_entity_len
 news_dict['<his>']['category'] = category_dict['<his>']
-news_dict['<his>']['subcate'] = subcategory_dict['<his>']
 
 print('news_num', len(news_dict))
 print('entity num', len(entity_dict))
 print('category num', len(category_dict))
-print('subcategory num', len(subcategory_dict))
 print('word num', len(word_dict))
 
 user_dict = {}
@@ -168,33 +147,23 @@ for uid, uinfo in tqdm(user_dict.items(), total=len(user_dict), desc='parse user
     cate_dict = {}
     for h in uinfo['hist']:
         cate = news_dict[h]['category']
-        subcate = news_dict[h]['subcate']
         idx = news_dict[h]['idx']
         if cate not in cate_dict:
-            cate_dict[cate] = {}
-        if subcate not in cate_dict[cate]:
-            cate_dict[cate][subcate] = []
-        cate_dict[cate][subcate].append(idx)
+            cate_dict[cate] = []
+        cate_dict[cate].append(idx)
     
     uinfo['cate_dict'] = cate_dict
     
     cate_num_list.append(len(cate_dict))
+    total_num = 0
     for ck, cd in cate_dict.items():
-        total_num = 0
-        subcate_num_list.append(len(cd))
-        for sbk, sbl in cd.items():
-            subcate_news_list.append(len(sbl))
-            total_num += len(sbl)
-        cate_news_list.append(total_num)
+        total_num += len(cd)
+    cate_news_list.append(total_num)
 
 avg_cate_num = sum(cate_num_list) / len(cate_num_list)
-avg_subcate_num = sum(subcate_num_list) / len(subcate_num_list)
 avg_cate_news = sum(cate_news_list) / len(cate_news_list)
-avg_subcate_news = sum(subcate_news_list) / len(subcate_news_list)
 print('avg category per user', avg_cate_num)
-print('avg subcategory per category', avg_subcate_num)
 print('avg news number per category', avg_cate_news)
-print('avg news number per subcategory', avg_subcate_news)
 
 print('save dicts')
 json.dump(user_dict, open(os.path.join(data_path, "user.json"), 'w', encoding='utf-8'))
@@ -202,7 +171,6 @@ json.dump(news_dict, open(os.path.join(data_path, "news.json"), 'w', encoding='u
 json.dump(word_dict, open(os.path.join(data_path, "word.json"), 'w', encoding='utf-8'))
 json.dump(entity_dict, open(os.path.join(data_path, "entity.json"), 'w', encoding='utf-8'))
 json.dump(category_dict, open(os.path.join(data_path, "category.json"), 'w', encoding='utf-8'))
-json.dump(subcategory_dict, open(os.path.join(data_path, "subcategory.json"), 'w', encoding='utf-8'))
 
 
 
