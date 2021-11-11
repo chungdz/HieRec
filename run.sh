aa4e768dc9d3b3a57de987a46f19e49a0d854155
xiaoxin Hu <1033718596@qq.com>
2021-11-11 11:13:32 -0800

    direct parse test

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ python -m prepocess.build_dicts
 python -m prepocess.embd
 python -m prepocess.user_embd
 python -m prepocess.build_train --processes=10
-python -m prepocess.build_dev --processes=10
+python -m prepocess.build_dev --processes=10 --fsamples=test/behaviors.tsv
 CUDA_VISIBLE_DEVICES=0,1,2,3 python training.py --gpus=4 --epoch=10
 CUDA_VISIBLE_DEVICES=0,1,2,3 python validate.py --gpus=4 --epoch=10 --filenum=10
