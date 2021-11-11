34e89701e76b14522275970fa793bebb6687093c
xiaoxin Hu <1033718596@qq.com>
2021-11-11 10:34:28 -0800

    no test

--- run.sh
+++ run.sh
@@ -3,6 +3,5 @@ python -m prepocess.embd
 python -m prepocess.user_embd
 python -m prepocess.build_train --processes=10
 python -m prepocess.build_dev --processes=10
-python -m prepocess.build_test --processes=20
 CUDA_VISIBLE_DEVICES=0,1,2,3 python training.py --gpus=4 --epoch=10
-CUDA_VISIBLE_DEVICES=0,1,2,3 python validate.py --gpus=4 --epoch=10 --filenum=20
+CUDA_VISIBLE_DEVICES=0,1,2,3 python validate.py --gpus=4 --epoch=10 --filenum=10
